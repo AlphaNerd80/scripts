c28686071e95f605942e978ed613f7828ed62f37
Steven Mustafa <steven.i.mustafa@gmail.com>
2015-08-18 13:47:53 -0600

    added twos scripts

--- topwatch.sh
+++ topwatch.sh
@@ -7,10 +7,15 @@
 #               top.  In the script below, <job-name> has to be changed 
 #				to the actual name of the job.
 #				The script should be put in cron and run as follows:
-#               topwatch
 #
+#               topwatch >> logfile
+#
+#				The columns that matter the most for memory are:
+#               Col 5 (VIRT), Col 6 (RES), Col 7 (%CPU) and Col 8 (%MEM)
+#
+#				To add to cron: http://www.cyberciti.biz/faq/how-do-i-add-jobs-to-cron-under-linux-or-unix-oses/
 ######################################################################
 
 
-top -b -n 1 | grep --line-buffered crond
+top -b -n 1 | grep --line-buffered <job-name>
 exit
